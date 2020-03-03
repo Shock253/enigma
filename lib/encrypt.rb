72e2b7cab4ed50c7abeb52261ee2147771a7a03a
Ethan Hocking <eshocking253@gmail.com>
2020-03-02 21:45:10 -0700

    Add output line for key and date to encrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -11,3 +11,5 @@ new_file = File.open(ARGV[1], "w")
 encrypted_hash = enigma.encrypt(message)
 new_file.write(encrypted_hash[:encryption])
 new_file.close
+
+puts "Created '#{ARGV[0]}' with the key #{encrypted_hash[:key]} and date #{encrypted_hash[:date]}"

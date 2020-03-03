24bb0b38430f669c7b869608ebd8a97c61997861
Ethan Hocking <eshocking253@gmail.com>
2020-03-02 21:47:27 -0700

    Add decrypt.rb (requires all 4 args)

---
+++ lib/decrypt.rb
@@ -0,0 +1,13 @@
+require_relative "enigma"
+
+new_file = File.open(ARGV[0], "r")
+message = new_file.read.split("\n").join(" ")
+new_file.close
+
+enigma = Enigma.new
+
+new_file = File.open(ARGV[1], "w")
+
+decrypted_hash = enigma.decrypt(message, ARGV[2], ARGV[3])
+new_file.write(decrypted_hash[:decryption])
+new_file.close
